869295cace860c1be2db701b672f44c8313b07d0
Matthew <xuwanjin1991@Gmail.com>
2021-02-13 10:45:32 +0800

    Activity 添加到清单文件
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

--- Ominous/src/main/java/me/xuwanjin/ominous/activity/OminousSettingsActivity.kt
+++ Ominous/src/main/java/me/xuwanjin/ominous/activity/OminousSettingsActivity.kt
@@ -2,10 +2,21 @@ package me.xuwanjin.ominous.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 
 class OminousSettingsActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            ShowText()
+        }
     }
+}
+@Preview
+@Composable
+fun ShowText(){
+    Text(text = "Matthew")
 }
--- app/src/main/java/me/xuwanjin/ominousdemo/MainActivity.kt
+++ app/src/main/java/me/xuwanjin/ominousdemo/MainActivity.kt
@@ -1,15 +1,17 @@
 
 package me.xuwanjin.ominousdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Process
+import me.xuwanjin.ominous.activity.OminousSettingsActivity
 import me.xuwanjin.ominous.catcher.OminousCatcher
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        startActivity(Intent(this, OminousSettingsActivity::class.java))
     }
 }
